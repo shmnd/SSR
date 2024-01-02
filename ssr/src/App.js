01b48201b97c28dfbe323ef706d03bc9ce046ad3
“shamnad” <hamzashamnad123@gmail.com>
2024-01-02 12:29:18 +0530

    render return html method

--- ssr/src/App.js
+++ ssr/src/App.js
@@ -4,20 +4,35 @@ import About from './container/About';
 import Profile from './container/Profile';
 import {BrowserRouter as Router,Routes, Route ,Link} from 'react-router-dom'
 
+// function App() {
+//   // const [state,setState]=useState('')
+//   return (
+//     <div className='App'>
+//       <Router>
+//         <Link to='/about' > About page</Link>
+//         <Link to='/profile'> Profile page </Link>
+//         <Routes>
+//           <Route Component={About} path='/about' />
+//           <Route Component={Profile} path='/profile' />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+/////////////////////////////////////////////////////USING RENDERING METHOD//////////////////////////////////////
+
 function App() {
-  // const [state,setState]=useState('')
-
-
-
-  
   return (
     <div className='App'>
       <Router>
         <Link to='/about' > About page</Link>
         <Link to='/profile'> Profile page </Link>
         <Routes>
-          <Route Component={About} path='/about' />
-          <Route Component={Profile} path='/profile' />
+          <Route path='/about' element={'hi everyone this is about page '} />
+          <Route path='/profile' element={'hello guys this is profile page'} />
         </Routes>
       </Router>
     </div>
