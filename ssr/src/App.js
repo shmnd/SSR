5eed039f0e370a90767edea4335b65ebbd51bc8b
“shamnad” <hamzashamnad123@gmail.com>
2024-01-02 11:38:45 +0530

    using router

--- ssr/src/App.js
+++ ssr/src/App.js
@@ -1,29 +1,31 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import './App.css';
 import About from './container/About';
 import Profile from './container/Profile';
+import {BrowserRouter as Router,Routes, Route ,Link} from 'react-router-dom'
 
 function App() {
-  const [state,setState]=useState('')
+  // const [state,setState]=useState('')
 
 
-  let check
 
-  if (state==='about'){
-    check=<About></About>
-  }
-  else if( state==='profile'){
-    check=<Profile></Profile> 
-  }
   
   return (
     <div className='App'>
-      <button onClick={()=>setState('about')}> About</button>
-      <button onClick={()=>setState('profile')} > Profile</button>
-      {check}
-      
+      <Router>
+        <Link to='/about' > About page</Link>
+        <Link to='/profile'> Profile page </Link>
+        <Routes>
+          <Route Component={About} path='/about' />
+          <Route Component={Profile} path='/profile' />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
+
--- ssr/src/container/About.jsx
+++ ssr/src/container/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h1>About Me</h1>
+      <h1>About Me (using 2 methode)</h1>
     </div>
   )
 }
